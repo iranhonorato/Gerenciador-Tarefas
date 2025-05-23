59183dc62d72b0b8f143a55c9f041af2afeb80ed
iranhonorato <antonioihsj@al.insper.edu.br>
2025-05-23 16:49:00 -0300

    Início do projeto

---
+++ src/Components/AddTask.jsx
@@ -0,0 +1,9 @@
+import classes from "../Styles/AddTask.module.css"
+
+
+
+export default function AddTask() {
+    return (
+        <h1>Hello World</h1>
+    )
+}
---
+++ src/Components/TaskList.jsx
@@ -0,0 +1,9 @@
+import classes from "../Styles/TaskList.module.css"
+
+
+
+export default function TaskList() {
+    return(
+        <h1>Hello World</h1>
+    )
+}
