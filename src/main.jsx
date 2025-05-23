445790f6d0161809d0355cb705637b534b6f8235
iranhonorato <antonioihsj@al.insper.edu.br>
2025-05-23 16:28:12 -0300

    Reestruturando pastas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './Styles/index.css'
+import App from './Pages/App.jsx'
+
+
+const routerList = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routerList}/>
   </StrictMode>,
 )
