6a47852ab89896a2755f9d7708c0a6280dce2201
iranhonorato <antonioihsj@al.insper.edu.br>
2025-05-23 17:00:04 -0300

    Rascunho dos componentes

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
-import '../Styles/App.module.css'
+import classes from '../Styles/App.module.css'
 
 function App() {
 
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className={classes.principal}>
+      <div className={classes.secundario}>
+        <h1>AddTask</h1>
+      </div>
+      <div className={classes.secundario}>
+        <h1>TaskList</h1>
+      </div>
     </div>
   )
 }
